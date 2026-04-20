b6f87e508da0f99a62133fac656fc8a28e09e7eb
davidrlopez <screen.mp3@gmail.com>
2026-04-20 16:30:09 +0200

    add db in CI and test fixes

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    env: {
-      DATABASE_URL:
-        "postgresql://lmbd_JDQcT8YBsp:RuWPVC0FFB8qbKUvQZKl@localhost:5432/lambda",
-    },
-  },
+  test: {},
 });
